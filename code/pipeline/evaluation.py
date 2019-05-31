fe3fba905d36bf46472110ca7cc778fd1c503941
satej soman <satej.soman@gmail.com>
2019-05-31 00:02:55 -0500

    bugs found in hw5

--- code/pipeline/evaluation.py
+++ code/pipeline/evaluation.py
@@ -1,8 +1,12 @@
 import numpy as np
 from sklearn.metrics import (accuracy_score, f1_score, precision_recall_curve,
                              precision_score, recall_score, roc_auc_score)
-
+from sklearn.svm import LinearSVC
 # taken in large part from rayid ghani's magicloops 
+
+score_function_overrides = {
+    LinearSVC: LinearSVC.decision_function
+}
 
 metrics = {
     "precision": precision_score,
