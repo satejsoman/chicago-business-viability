68f3dacb06ca611a5167867276968533be37dcd1
satej soman <satej.soman@gmail.com>
2019-06-02 00:20:57 -0500

    major features

--- code/data-assembly/cdp_joins.py
+++ code/data-assembly/cdp_joins.py
@@ -1,10 +1,10 @@
 # script to join together various business attributes from CDP datasets
-# [x] number of previous license renewals 
+# [X] number of previous license renewals 
 # [X] number of unique sites 
 # [X] in SSA (binary)
 # [X] which SSA (categorical)
-# [ ] Number of days between LICENSE APPROVED FOR ISSUANCE and DATE ISSUED
-# [x] Number of business owned by owner
+# [X] Number of days between LICENSE APPROVED FOR ISSUANCE and DATE ISSUED
+# [X] Number of business owned by owner
 # [ ] received TIF
 # [ ] Received Small Business Improvement Fund (SBIF) grant
 # [d] is national chain
@@ -16,26 +16,23 @@ from tqdm import tqdm
 
 tqdm.pandas()
 
-# national_chain_list = [
-#     "WALGREEN CO.", "BOND DRUG COMPANY OF ILLINOIS, LLC", "SP PLUS CORPORATION", 
-#     "STARBUCKS CORPORATION", "AMERICAN DRUG STORES LLC", "FAMILY DOLLAR, INC.", 
-#     "HIGHLAND PARK CVS, L.L.C."
-# ]
-
 # def is_national_chain(df):
 #     df["is_national_chain"] = df["LEGAL NAME"].isin(national_chain_list)
 
 def process_datetimes(df):
-    # dt_cols = [col for col in df.columns if 'DATE' in col]
-    # dt_cols = ['LICENSE APPROVED FOR ISSUANCE', 'DATE ISSUED']
-    df["YEAR"] = df["DATE ISSUED"].progress_apply(lambda row: pd.to_datetime(row).year)
+    dt_cols = ['LICENSE APPROVED FOR ISSUANCE', 'DATE ISSUED']
+    for col in dt_cols:
+        df[col] = df[col].progress_apply(lambda row: pd.to_datetime(row, errors="coerce"))
+    df["YEAR"] = df["DATE ISSUED"].dt.year
+    df["days_between_approval_issuance"] = (df['DATE ISSUED'] - df['LICENSE APPROVED FOR ISSUANCE']).dt.days.fillna(0).astype(int)
     return df
 
 def process_business_owners(df):
-    df[["Owner First Name", "Owner Middle Initial", "Owner Last Name"]] = df[["Owner First Name", "Owner Middle Initial", "Owner Last Name"]].fillna(" ")
-    df["full_name"] = df["Owner First Name"] + " " + df["Owner Middle Initial"] + " " + df["Owner Last Name"]
-    df["num_accounts"] = df["full_name"].progress_map(df["full_name"].value_counts())
-    return df 
+    df[["Owner First Name", "Owner Middle Initial", "Owner Last Name", "Legal Entity Owner"]] = df[["Owner First Name", "Owner Middle Initial", "Owner Last Name", "Legal Entity Owner"]].fillnan("")
+    df["full_name"] = (df["Owner First Name"] + " " + df["Owner Middle Initial"] + " " + df["Owner Last Name"] + " " + df["Legal Entity Owner"]).replace('\s+', ' ', regex=True)
+    df_filtered = df[~(df["full_name"].isin(["Business Owner", ""]))]
+    df_filtered["num_businesses_on_account"] = df_filtered["full_name"].map(df["full_name"].value_counts())
+    return df_filtered[["Account Number", "num_businesses_on_account"]]
  
 def get_num_renewals_map(df):
     # subsequent renewal and license granted
@@ -53,17 +50,20 @@ def non_join_transformations(df):
 def main(data_path, licenses, owners):
     # licenses = process_datetimes(licenses)
     licenses = non_join_transformations(process_datetimes(licenses))
-    # owners = process_business_owners(owners)
-    # owners.rename(str.upper, axis="columns", inplace=True)
-    licenses[["ACCOUNT NUMBER", "SITE NUMBER", "YEAR",'which_ssa', 'in_ssa', 'num_sites']].to_csv(data_path/'licenses_joined.csv')
-
-if __name__ == "__main__":
-    data = Path("./data")
-    licenses_path   = data/"Business_Licenses.csv"
-    owners_path     = data/"Business_Owners.csv"
-    fortune1k_path  = data/"fortune1000.csv"
+    renewal_counts = get_num_renewals_map(licenses)
+    licenses["num_renewals"] = licenses["license_site"].map(renewal_counts).fillna(0)
     
-    main(
-        data_path = data,
-        licenses  = pd.read_csv(licenses_path), 
-        owners    = pd.read_csv(owners_path))
+    licenses = licenses.merge(process_business_owners(owners), left_on="ACCOUNT NUMBER", right_on="Account Number")
+    
+    licenses[["ACCOUNT NUMBER", "SITE NUMBER", "YEAR",'which_ssa', 'in_ssa', 'num_sites', 'num_renewals']].to_csv(data_path/'licenses_joined.csv')
+
+# if __name__ == "__main__":
+#     data = Path("./data")
+#     licenses_path   = data/"Business_Licenses.csv"
+#     owners_path     = data/"Business_Owners.csv"
+#     fortune1k_path  = data/"fortune1000.csv"
+    
+#     main(
+#         data_path = data,
+#         licenses  = pd.read_csv(licenses_path), 
+#         owners    = pd.read_csv(owners_path))
