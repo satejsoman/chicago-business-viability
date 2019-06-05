042f63757872bd59f5f723535bdbbbd68733df11
satej soman <satej.soman@gmail.com>
2019-06-05 18:05:14 -0500

    last of the merge conflicts

--- code/chicago-business.py
+++ code/chicago-business.py
@@ -19,33 +19,13 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
 from pipeline.core import Pipeline
-<<<<<<< HEAD
 from pipeline.transformation import (Transformation, to_datetime)
-=======
-from pipeline.transformation import (Transformation, binarize, categorize,
-                                     hash_string, replace_missing_with_value,
-                                     scale_by_max, to_datetime)
->>>>>>> first-model
 from pipeline.grid import Grid
 
 from data_cleaning import (clean_data, filter_out_2019_data)
 from feature_generation import (make_features, reshape_and_create_label,
                                 count_by_zip_year, count_by_dist_radius,
-<<<<<<< HEAD
                                 balance_features, make_dummy_vars)
-=======
-                                balance_features)
-
-def explore():
-    pass
-
-def clean():
-    pass
-
-def predict():
-    pass
-
->>>>>>> first-model
 
 def clean_chicago_business_data(self):
     self.logger.info("    Running cleaning steps on raw data")
@@ -71,35 +51,12 @@ def make_chicago_business_features(self):
         self.logger.info("    Balancing features for test-train set %s", i+1)
         self.train_sets[i], self.test_sets[i] = balance_features(self.train_sets[i], self.test_sets[i])
 
-<<<<<<< HEAD
-=======
     # Add newly-generated features to self.features
     new_cols = set(self.test_sets[0].columns)
     self.features = list(new_cols - old_cols) # set difference
-    print(self.features)
 
->>>>>>> first-model
     return self
 
-
-# def get_pipeline(config, original_df=original_df):
-#     pipeline = Pipeline(
-#         original_df,
-#         "not_renewed_2yrs",
-#         data_cleaning = [
-#             to_datetime("LICENSE TERM EXPIRATION DATE"),
-#             to_datetime("DATE ISSUED")
-#         ],
-#         feature_generators=[
-#             count_by_zip_year,
-#             count_by_dist_radius
-#         ],
-#         model_grid=Grid.from_config(config["models"]),
-#         name="quick-pipeline-lr-only",
-#         output_root_dir=Path("output/"))
-
-#     pipeline.generate_features = MethodType(make_chicago_business_features, pipeline)
-#     return pipeline
 
 def main(config_path):
     script_dir = Path(__file__).parent
@@ -114,7 +71,6 @@ def main(config_path):
     model_grid    = Grid.from_config(config["models"])
 
     pipeline = Pipeline(
-<<<<<<< HEAD
         input_source    = input_path,
         target          = target,
         name            = pipeline_name,
@@ -132,22 +88,6 @@ def main(config_path):
         ])
 
     pipeline.clean_data        = MethodType(clean_chicago_business_data, pipeline)
-=======
-        Path(config["data"]["input_path"]),
-        config["pipeline"]["target"],
-        data_cleaning=[
-            to_datetime("LICENSE TERM EXPIRATION DATE"),
-            to_datetime("DATE ISSUED")
-        ],
-        feature_generators=[
-            count_by_zip_year,
-            count_by_dist_radius
-        ],
-        summarize=False,
-        model_grid=Grid.from_config(config["models"]),
-        name="quick-pipeline-lr-only-" + config["description"],
-        output_root_dir=Path("output/"))
->>>>>>> first-model
     pipeline.generate_features = MethodType(make_chicago_business_features, pipeline)
     pipeline.run()
 
