3012af55d700a60075cc7e2ef1576dd21df9724a
Jonathan Tan <jon.twk@gmail.com>
2019-06-06 20:57:02 -0500

    fixed sh typos and added echo

--- setup.sh
+++ setup.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # Make and load environment
+echo "Making environment"
 module load python/3.6.1+intel-16.0
 python3 -mvenv env
 source env/bin/activate
 
 # Install requirements
+echo "Installing requirements"
 module load python/3.6.1+intel-16.0
 pip3 install --upgrade pip
-pip3 install -r requirements.txt
+pip3 install -r code/requirements.txt
 
 # Assemble data
+echo "Assembling data"
 gunzip data/Business_Licenses.csv.gz
 gunzip data/merged_business_govtdata.csv.gz
 python3 code/data-assembly/glue.py
