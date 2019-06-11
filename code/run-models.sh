a6ea35688d3a33aaff6e1ac3e847692aa393c7d5
Cecile Murray <29072007+murray-cecile@users.noreply.github.com>
2019-06-10 23:24:29 -0500

    tweaks to PR

--- code/run-models.sh
+++ code/run-models.sh
@@ -4,8 +4,8 @@
 # time python chicago-business.py --config config2.yml
 # time python chicago-business.py --config config3.yml # stopped at 2015
 
-time python chicago-business.py --config bag-config1.yml
-time python chicago-business.py --config bag-config2.yml
+# time python chicago-business.py --config bag-config1.yml
+# time python chicago-business.py --config bag-config2.yml
 time python chicago-business.py --config bag-config3.yml
 time python chicago-business.py --config bag-config4.yml
 time python chicago-business.py --config bag-config5.yml
